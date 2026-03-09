4bd11227d1396063b9fd873c1f08d6fbd954d212
calv004 <calvin.roth04@gmail.com>
2026-03-09 12:21:46 +0100

    implemented file encryption

--- IronCrypt.py
+++ IronCrypt.py
@@ -9,27 +9,39 @@ if len(sys.argv) != 2:
 key = get_random_bytes(32)
 
 def encrypt(text, key):
-
     cipher = AES.new(key, AES.MODE_GCM)
     ciphertext, tag = cipher.encrypt_and_digest(text)
 
     nonce = cipher.nonce
-
-    print(ciphertext)
     return nonce, tag, ciphertext
 
-def decrypt(text, key, nonce):
+def decrypt(text, key, nonce, tag):
     cipher_dec = AES.new(key, AES.MODE_GCM, nonce=nonce)
     plaintext = cipher_dec.decrypt_and_verify(text, tag)
+    return plaintext
 
-    print(plaintext.decode())
+def read_file(path):
+    with open(path, "rb") as file:
+        file_content = file.read()
+    return file_content
 
+def write_file(path, content):
+    with open(path, "wb") as file:
+        file.write(content)
 
 if __name__ == '__main__':
     key = get_random_bytes(32)
 
-    mytext = sys.argv[1]
-    bytes_text = mytext.encode()
+    file_path = sys.argv[1]
+
+    bytes_text = read_file(file_path)
+
     nonce, tag, ciphertext = encrypt(bytes_text, key)
 
-    decrypt(ciphertext, key, nonce)
+    write_file(sys.argv[1], ciphertext)
+
+    input("File got encrypted, press enter to decrypt")
+
+
+    decrypted_output = decrypt(ciphertext, key, nonce, tag)
+    write_file(sys.argv[1], decrypted_output)
